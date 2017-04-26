76872848bd391ae35d16ec759b852f7ed360de57
fang19911030 <fang291831388@gmail.com>
2017-04-26 14:50:03 -0400

    Update

--- TrappingWater.java
+++ TrappingWater.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class TrappingWater{
     public int trap(int[] height) {
         if(height == null || height.length==0){
             return 0;
@@ -43,5 +43,41 @@ public class Solution {
         }
         return res;
         
+    }
+    private void getLeft(int[]height, int[] left){
+        left[0] = -1;
+        for(int i=1;i<height.length;i++){
+            left[i] = Math.max(left[i-1],height[i-1]);
+        }
+    }
+    
+    private void getRight(int[] height, int[] right){
+        right[height.length-1] = -1;
+        for(int i=height.length-2;i>=0;i--){
+            right[i] = Math.max(height[i+1],right[i+1]);
+        }
+    }
+    public int trap2(int[] height) {
+        if(height == null || height.length==0){
+            return 0;
+        }
+        int res = 0;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        getLeft(height,left);
+        getRight(height,right);
+        for(int i=0;i<height.length;i++){
+            int lb = left[i];
+            int rb = right[i];
+            if(lb == -1 || rb == -1){
+                continue;
+            }
+            
+            if(height[i]<lb && height[i] <rb){
+                res+= lb>=rb? rb-height[i]:lb-height[i];
+            }
+
+        }
+        return res;
     }
 }
