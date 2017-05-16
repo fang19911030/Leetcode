37d249e3bf81c0f867a0dce32c3a6468ec4aa351
fang19911030 <fang291831388@gmail.com>
2017-05-16 11:08:01 -0400

    The optimal solution is function2

--- NumberOfDigitOne.java
+++ NumberOfDigitOne.java
@@ -35,4 +35,20 @@ public class Solution {
         }
         return res;
     }
+    public int countDigitOne2(int n) {
+    
+        if (n <= 0) return 0;
+        int q = n, x = 1, ans = 0;
+        do {
+            int digit = q % 10;
+            q /= 10;
+            ans += q * x;
+            if (digit == 1) ans += n % x + 1;
+            if (digit >  1) ans += x;
+            x *= 10;
+        } while (q > 0);
+        return ans;
+    
+    }
+
 }
