726c0026d0d1b6a654bc9384a6fe21b8bd4d2603
fang19911030 <fang291831388@gmail.com>
2017-05-18 13:12:02 -0400

    ADD BETTER SOLUTION

--- FindAllDuplicatesInArray.java
+++ FindAllDuplicatesInArray.java
@@ -12,4 +12,16 @@ public class Solution {
         
         return res;
     }
+/*     when find a number i, flip the number at position i-1 to negative.  1 =<nums[i]<=n(n size of array)
+     if the number at position i-1 is already negative, i is the number that occurs twice.*/
+    public List<Integer> findDuplicates2(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index+1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
 }
