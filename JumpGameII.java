1ed7a80756b40411e0e22ff12687d00ad0603780
fang19911030 <fang291831388@gmail.com>
2017-05-30 20:42:34 -0400

    The best solution is based on the Greedy.

--- JumpGameII.java
+++ JumpGameII.java
@@ -16,4 +16,20 @@ public class Solution {
         }
         return res;
     }
-}
+
+    public int jump2(int[] A) {
+        int jumps = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < A.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + A[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+
+                if (curEnd >= A.length - 1) {
+                    break;
+                }
+            }
+        }
+        return jumps;
+    }
+}                 //TLE
