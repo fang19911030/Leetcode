23eeb969a269d6bc1ac503456ed361c9eaf52be9
fang19911030 <fang291831388@gmail.com>
2017-06-13 16:39:07 -0400

    add solution for 397

---
+++ IntegerRelacement.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int integerReplacement(int n) {
+        if(n==1){
+            return 0;
+        }
+        if(n%2 == 0){
+            return 1+integerReplacement(n/2);
+        }else{
+            return (2 + Math.min(integerReplacement(n/2), integerReplacement(n/2 + 1)));
+        }
+    }
+}
