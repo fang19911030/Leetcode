9c3c4c96af23eb6022daf2d57f35a1aacc78dedd
fang19911030 <fang291831388@gmail.com>
2017-06-17 10:52:57 -0400

    The second one is better

--- RotateFunction.java
+++ RotateFunction.java
@@ -17,4 +17,20 @@ public class Solution {
         return res;
 
     }
+
+    public int maxRotateFunction2(int[] A) {
+        int allSum = 0;
+        int len = A.length;
+        int F = 0;
+        for (int i = 0; i < len; i++) {
+            F += i * A[i];
+            allSum += A[i];
+        }
+        int max = F;
+        for (int i = len - 1; i >= 1; i--) {
+            F = F + allSum - len * A[i];
+            max = Math.max(F, max);
+        }
+        return max;   
+    }
 }
