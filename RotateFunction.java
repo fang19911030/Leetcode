9fad4f8a456b26bfae0ffa3a5f65d050379f5a12
fang19911030 <fang291831388@gmail.com>
2017-06-17 10:57:58 -0400

    Add comment

--- RotateFunction.java
+++ RotateFunction.java
@@ -1,3 +1,24 @@
+/*
+F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1]
+F(k-1) = 0 * Bk-1[0] + 1 * Bk-1[1] + ... + (n-1) * Bk-1[n-1]
+       = 0 * Bk[1] + 1 * Bk[2] + ... + (n-2) * Bk[n-1] + (n-1) * Bk[0]
+Then,
+
+F(k) - F(k-1) = Bk[1] + Bk[2] + ... + Bk[n-1] + (1-n)Bk[0]
+              = (Bk[0] + ... + Bk[n-1]) - nBk[0]
+              = sum - nBk[0]
+Thus,
+
+F(k) = F(k-1) + sum - nBk[0]
+What is Bk[0]?
+
+k = 0; B[0] = A[0];
+k = 1; B[0] = A[len-1];
+k = 2; B[0] = A[len-2]
+
+*/
+
+
 public class Solution {
     public int maxRotateFunction(int[] A) {
         
