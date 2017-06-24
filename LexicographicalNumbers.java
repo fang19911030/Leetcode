b86204afbf9a810884055af123c18980102f2d30
fang19911030 <fang291831388@gmail.com>
2017-06-24 10:48:02 -0400

    add new solution

--- LexicographicalNumbers.java
+++ LexicographicalNumbers.java
@@ -14,4 +14,45 @@ public class Solution {
         }
         return res;
     }
+
+    public List<Integer> lexicalOrder(int n){
+        List<Integer> res = new ArrayList<>();
+        for(int i=1;i<10;++i){
+            dfs(i, n,res);
+        }
+        return res;
+    }
+
+    private void dfs(int cur, int n,List<Integer> res){
+        if(cur>n){
+            return;
+        }else{
+            res.add(cur);
+            for(int i=0;i<10;i++){
+                if(10*cur+i>n){
+                    return;
+                }
+                dfs(10*cur+i,n,res);
+            }
+        }
+    }
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        int curr = 1;
+        for (int i = 1; i <= n; i++) {
+            list.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else if (curr % 10 != 9 && curr + 1 <= n) {
+                curr++;
+            } else {
+                while ((curr / 10) % 10 == 9) {
+                    curr /= 10;
+                }
+                curr = curr / 10 + 1;
+            }
+        }
+        return list;
+    }
 }
